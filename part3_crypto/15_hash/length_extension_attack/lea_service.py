f8620fb3991413263b55a74cd0542af4be3442c1
takuto-yamamoto <takuto.yamamoto.dev@gmail.com>
2025-01-17 16:54:28 +0900

    add: hash

---
+++ part3_crypto/15_hash/length_extension_attack/lea_service.py
@@ -0,0 +1,33 @@
+import hashlib
+import sys
+from binascii import hexlify, unhexlify
+
+SALT = b"secretsalt"
+
+# --------------------------------------------------
+print("[Register]")
+username = input("username> ").encode()
+if b"=" in username:
+    print("Bye.")
+    sys.exit(1)
+
+userdata = b"priv=guest,username=" + username
+h = hashlib.md5(SALT + userdata).hexdigest()
+print("Your userdata(hex) is:", hexlify(userdata).decode())
+print("Your signature is:", h)
+
+# --------------------------------------------------
+print("[Login]")
+userdata = unhexlify(input("userdata(hex)> "))
+sig = input("signature(hex)> ")
+h = hashlib.md5(SALT + userdata).hexdigest()
+if h == sig:
+    print("Login OK!")
+
+    if b"priv=admin" in userdata.split(b","):
+        print("Congratulations! You are admin!")
+    else:
+        print("Sorry, admin only")
+else:
+    print("Bye.")
+    sys.exit(1)
